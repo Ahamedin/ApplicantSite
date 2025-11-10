fecaa612986905b9934760b687c5575f94360434
Ahamedin <iklashriz@gmail.com>
2025-11-10 21:30:12 +0530

    fix prisma esm import

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 const __dirname = path.resolve();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true, }));    
+app.use(cors({ origin: '*', credentials: true, }));    
 app.use(express.json())
 app.use(rateLimiter)
 app.use(session({
--- backend/utilities/connectpsql.js
+++ backend/utilities/connectpsql.js
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+// backend/utilities/connectpsql.js
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
 const prisma = new PrismaClient();
 
